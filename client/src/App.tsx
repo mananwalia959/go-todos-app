748fda6792697682241d8bcef50c3967d7ccc6ce
Manan Walia <mananwalia959@gmail.com>
2021-07-24 20:33:15 +0530

    feat : validate token validity at app startup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,22 +5,40 @@ import TodosList from './components/TodosList';
 import LoginCallback from './components/auth/LoginCallback';
 import LoginPage from './components/auth/LoginPage';
 import AuthContext from './contexts/AuthContext';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { authService } from './services/auth-service';
 import StorageService from './services/StorageService';
 import ProtectedRoute from './routing/ProtectedRoutes';
+import { UserPrincipal } from './models/auth/UserPrincipal';
 
 function App() {
-    const [token, setToken] = useState(StorageService.getToken() || '');
-    const [userPrincipal, setUserPrincipal] = useState(
-        authService.tokenToUserPrincipal(token)
-    );
+    const [token, setToken] = useState('');
+    const [userPrincipal, setUserPrincipal] = useState({} as UserPrincipal);
+    const [isLoaded, setLoaded] = useState(false);
 
     const setTokenAndUserPrincipal = (tkn: string) => {
         StorageService.setToken(tkn);
         setToken(tkn);
         setUserPrincipal(authService.tokenToUserPrincipal(tkn));
     };
+
+    useEffect(() => {
+        const tkn = StorageService.getToken() || '';
+        // no need to make calls if there is no token
+        if (!tkn) {
+            setLoaded(true);
+            return;
+        }
+        console.log('here');
+
+        authService
+            .isTokenValidTillNextDay(tkn)
+            .then(() => {
+                setToken(tkn);
+                setUserPrincipal(authService.tokenToUserPrincipal(tkn));
+            })
+            .finally(() => setLoaded(true));
+    }, []);
 
     return (
         <>
@@ -32,20 +50,22 @@ function App() {
                 }}
             >
                 <AppHeader />
-                <Container maxW="container.md" p="2">
-                    <Switch>
-                        <ProtectedRoute path="/" exact component={TodosList} />
-                        <Route path="/login" exact component={LoginPage} />
 
-                        <Route
-                            path="/callback/googleoauth"
-                            component={LoginCallback}
-                        />
-                    </Switch>
-                </Container>
+                {isLoaded ? <Routes /> : ''}
             </AuthContext.Provider>
         </>
     );
 }
+
+const Routes = () => (
+    <Container maxW="container.md" p="2">
+        <Switch>
+            <ProtectedRoute path="/" exact component={TodosList} />
+            <Route path="/login" exact component={LoginPage} />
+
+            <Route path="/callback/googleoauth" component={LoginCallback} />
+        </Switch>
+    </Container>
+);
 
 export default App;
--- client/src/services/auth-service.ts
+++ client/src/services/auth-service.ts
@@ -7,6 +7,7 @@ interface AuthService{
     getToken(code :string): Promise<TokenResponse>
     getOauthUrl(): Promise<any>
     tokenToUserPrincipal(token:string): UserPrincipal
+    isTokenValidTillNextDay(token:string):Promise<any>
 }
 
 class AuthServiceImpl implements AuthService {
@@ -17,6 +18,11 @@ class AuthServiceImpl implements AuthService {
     getOauthUrl(): Promise<any> {
         return axios.get<OauthResponse>('/api/auth/loginurl/google')
             .then(res => window.location.href = (res.headers.location));
+    }
+    //i can check expired field locally but in case if the secret is rotated , 
+    //its better to validate this at the backend too 
+    isTokenValidTillNextDay(token:string):Promise<any>{
+        return axios.post<any>('/api/auth/validatetoken', {jwtToken: token})
     }
 
     tokenToUserPrincipal(token:string): UserPrincipal {
